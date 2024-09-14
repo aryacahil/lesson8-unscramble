ec8d7b18d8f269d922c86f6bcc337774e36e448a
aryacahil <campgreget2@gmail.com>
2024-09-14 13:37:52 +0700

    inisialisasi project

---
+++ app/src/main/java/com/example/android/unscramble/ui/game/ListofWords.kt
@@ -0,0 +1,30 @@
+package com.example.android.unscramble.ui.game
+
+
+const val MAX_NO_OF_WORDS = 10
+const val SCORE_INCREASE = 20
+
+// List with all the words for the Game
+val allWordsList: List<String> =
+    listOf("thoriq",
+        "arya",
+        "cecee",
+        "echa",
+        "charis",
+        "bentara",
+        "robby",
+        "nathan",
+        "dival",
+        "kevin",
+        "eric",
+        "ocha",
+        "syammanda",
+        "putri",
+        "biya",
+        "michi",
+        "asyel",
+        "ipek",
+        "shinta",
+        "swara",
+        "cessa",
+        "indy",)
